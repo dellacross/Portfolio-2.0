886262e2cc5600055baee7b99a7ef6043cbe4523
dellacross <victordella652@gmail.com>
2024-08-12 09:14:49 -0300

    React structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App;
